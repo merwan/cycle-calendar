196f65e51da7e8251ee0046bd4ceb81fc187c804
Merouane Atig <merouane.atig@gmail.com>
2014-10-30 07:49:38 +0100

    basic elements added to front page

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,22 +5,27 @@ feature 'Smoke test' do
     # Alice visit the website to help her remember her period dates
     visit('/')
 
-    # She notices the header mention periods
+    # She notices the title and header mention periods
+    expect(page).to have_title('Check your period')
     expect(find('h1')).to have_content('Check your period')
-    pending 'Finish the test'
 
     # She is invited to enter her period date straight away
-    #inputbox = find('#id_new_period')
-    #expect(inputbox[:placeholder]).to eq('Enter a period date')
+    inputbox = find('#id_new_period')
+    expect(inputbox[:placeholder]).to eq('Enter a period date')
 
     # She types her last period date into a text box
-    #fill_in('Period date', with: '2014-10-29')
+    fill_in('Period date', with: '2014-10-29')
 
     # When she hits enter, the page updates and lists
     # "2014-10-29" as a period date
+    table = find('#id_periods_table')
+    rows = table.find('tr')
+    expect(rows).to have_content('2014-10-29')
+
+    pending 'Finish the tests!'
 
     # There is still a text box to enter another period date
-    # Sshe enters another date
+    # She enters another date
     #fill_in('Period date', with: '2014-09-27')
 
     # The page updates the page and now lists both dates
